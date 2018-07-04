8475a1a00658b8babe8e7402287836040f2d17b8
Georg-Bauer <Herr.Georg.Bauer@outlook.de>
2018-07-05 00:06:56 +0800

    1.7 获取用户输入

---
+++ chp1/1.7.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+if __name__ == "__main__":
+    # input
+    x = input("x: ")
+    y = input("y: ")
+    print x, y, x * y
+    # raw_input
+    a = raw_input("a: ")
+    b = raw_input("b: ")
+    print a, b, "a is " + a
