538b276610f1a32e84c782606ef0c7452ed730c5
Georg-Bauer <Herr.Georg.Bauer@outlook.de>
2018-07-05 22:41:28 +0800

    1.8 函数

---
+++ chp1/1.8.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+if __name__ == "__main__":
+    # print
+    x, y= 123, "world"
+    print "hello" + str(x) + y
+    print("hello" + y)
+    # pow
+    print 2 ** 3, pow(2, 4)
+    # abs
+    print abs(-10), abs(1.3)
+    # round
+    print round(1/2.0), round(-3.45)
